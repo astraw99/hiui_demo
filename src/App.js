8a3a92688c859ec7a92ec7b2f49df17a834e24dd
astraw99 <wangchengiscool@gmail.com>
2019-08-22 19:11:01 +0800

    hiui demo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Button } from '@hi-ui/hiui' //引入所需组件
+import logo from './logo.svg'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <div className='App'>
+                <header className='App-header'>
+                    <img src={logo} className='App-logo' alt='logo' />
+
+                    {/* 使用组件 */}
+                    <Button type='primary' size='large'>
+                        HIUI 按钮
+                    </Button>
+
+                    <p>
+                        Edit <code>src/App.js</code> and save to reload.
+                    </p>
+                    <a
+                        className='App-link'
+                        href='https://reactjs.org'
+                        target='_blank'
+                        rel='noopener noreferrer'
+                    >
+                        Learn React
+                    </a>
+                </header>
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
